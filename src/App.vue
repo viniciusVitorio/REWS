e46c1f0222c0300cdda0816e1df4706595cda6a7
Lucas Zambam <lucaszambam7896@gmail.com>
2024-10-31 18:41:22 -0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
@@ -21,6 +20,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h1>REWS</h1>
+        <span>O seu site de notícias sobre o rio</span>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped>
+
+    header {
+        color: white;
+        background-color: #0f434d;
+        padding: 25px;
+    }
+
+    h1 {
+        font-size: 50px;
+        margin: 0;
+    }
+
+    span {
+        font-size: 9.5px;
+        font-weight: 600;
+    }
+</style>
+
